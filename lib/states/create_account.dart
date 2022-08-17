8671950811c9d677a342f7db04d0a2b800cac3d0
Ohm <krittapas.artp@gmail.com>
2022-08-17 17:22:37 +0700

    first commit

---
+++ lib/states/create_account.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateAccoun extends StatefulWidget {
+  const CreateAccoun({Key? key}) : super(key: key);
+
+  @override
+  State<CreateAccoun> createState() => _CreateAccounState();
+}
+
+class _CreateAccounState extends State<CreateAccoun> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/utillity/my_constant.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyConstant {
+  //Genernal
+  static String appName = 'shoppingmall';
+
+  //Route
+
+  static String routeAuthen = '/authen';
+  static String routeCreateAccount = '/createAccount';
+  static String routeBuyerSiver = '/buyerService';
+  static String routeSalerService = '/salerService';
+  static String routeRiderService = '/riderService';
+}
